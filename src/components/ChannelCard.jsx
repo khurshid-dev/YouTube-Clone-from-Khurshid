23273f8956f4c49e92cdac2434eac5983134c5e4
Khurshid Uzokov <xurshiduzoqov1@gmail.com>
2023-01-16 16:57:45 +0500

    Update ChannelCard.jsx

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { MdOutlineDone } from 'react-icons/md';
+import { Link } from 'react-router-dom'
 
 const ChannelCard = ({ video }) => {
     return (
-        <div className='ChannelItem flex flex-col max-w-fit mr-6 mb-2 cursor-pointer hover:scale-90 transition-all'>
-            <div className='headerVideo cursor-pointer w-[142px] h-[136px] overflow-hidden rounded-full'>
-                <img src={video?.snippet?.thumbnails?.high?.url} className='relative bottom-[0] transition-all' alt={video?.snippet?.channelTitle} />
-            </div>
-            <div className='bodyVideo flex items-center pt-2 pb-1 mx-auto'>
-                <h1 className='text-[13px] leading-3 tracking-[-0.5px] font-semibold'>{video?.snippet?.channelTitle?.slice(0, 50)}</h1>
-                <div className='bg-primary rounded-full ml-1 p-[1px] text-white'><MdOutlineDone /></div>
-            </div>
-            <p className='text-gray-500 text-xs text-center'>{video?.snippet?.description.slice(0, 30)} ...</p>
+        <div>
+            <Link to={`/channel/${video?.snippet?.channelId}`}>
+                <div className='ChannelItem flex flex-col max-w-fit mr-6 mb-2 cursor-pointer hover:scale-90 transition-all'>
+                    <div className='headerVideo cursor-pointer w-[142px] h-[136px] overflow-hidden rounded-full'>
+                        <img src={video?.snippet?.thumbnails?.high?.url} className='relative bottom-[0] transition-all' alt={video?.snippet?.channelTitle} />
+                    </div>
+                    <div className='bodyVideo flex items-center pt-2 pb-1 mx-auto'>
+                        <h1 className='text-[13px] leading-3 tracking-[-0.5px] font-semibold'>{video?.snippet?.channelTitle?.slice(0, 50)}</h1>
+                        <div className='bg-primary rounded-full ml-1 p-[1px] text-white'><MdOutlineDone /></div>
+                    </div>
+                    <p className='text-gray-500 text-xs text-center'>{video?.snippet?.description.slice(0, 30)} ...</p>
+                </div>
+            </Link>
         </div>
+
     )
 }
 
